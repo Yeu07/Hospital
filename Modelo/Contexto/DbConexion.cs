6f546b22d8d3a0fd8ae933e5aad7a23f3dd876bb
Yeu <yeumensilva9@gmail.com>
2026-02-16 15:20:22 -0300

    Data Base Integration

---
+++ Modelo/Contexto/DbConexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Modelos
+{
+    public partial class DbConexion : DbContext
+    {
+
+        // constructor privado el cual usa el constructor de la clase base DbContext
+        private DbConexion(string connectionString)
+            : base(connectionString)
+        {
+            // Deshabilitar el Lazy Loading y Proxy Creation para evitar problemas de serialización 
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+            this.Database.CommandTimeout = 900;
+        }
+
+
+        public static DbConexion Create()
+        {
+            return new DbConexion("name=HospitalEntities");
+        }
+    }
+}
