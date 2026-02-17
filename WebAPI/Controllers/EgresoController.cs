35f16b4a6e4ad33866554bc4e0dcf98f9d726458
Yeu <yeumensilva9@gmail.com>
2026-02-17 12:34:32 -0300

    Paciente, Ingreso and Egreso Controllers

---
+++ WebAPI/Controllers/EgresoController.cs
@@ -0,0 +1,133 @@
+﻿using Comun.ViewModels;
+using Logica.BLL;
+using Modelo.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class EgresoController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult LeerTodo(int cantidad = 10, int pagina = 0, string textoBusqueda = null)
+        {
+            var respuesta = new RespuestaVMR<ListadoPaginadoVMR<EgresoVMR>>();
+
+            try
+            {
+                respuesta.datos = EgresoBLL.LeerTodo(cantidad, pagina, textoBusqueda);
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = null;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+        [HttpGet]
+        public IHttpActionResult LeerUno(long id)
+        {
+            var respuesta = new RespuestaVMR<EgresoVMR>();
+
+            try
+            {
+                respuesta.datos = EgresoBLL.LeerUno(id);
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = null;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+            if (respuesta.datos == null && respuesta.mensajes.Count() == 0)
+            {
+                respuesta.codigo = HttpStatusCode.NotFound;
+                respuesta.mensajes.Add("Elemento no encontrado");
+            }
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult Crear(Egreso item)
+        {
+            var respuesta = new RespuestaVMR<long?>();
+
+            try
+            {
+                respuesta.datos = EgresoBLL.Crear(item);
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = null;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(long id, EgresoVMR item)
+        {
+            var respuesta = new RespuestaVMR<bool?>();
+
+            try
+            {
+                item.id = id;
+                EgresoBLL.Actualizar(item);
+                respuesta.datos = true;
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = false;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+
+        [HttpDelete]
+        public IHttpActionResult Eliminar(List<long> ids)
+        {
+            var respuesta = new RespuestaVMR<bool?>();
+
+            try
+            {
+                EgresoBLL.Eliminar(ids);
+                respuesta.datos = true;
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = false;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+
+    }
+}
---
+++ WebAPI/Controllers/IngresoController.cs
@@ -0,0 +1,133 @@
+﻿using Comun.ViewModels;
+using Logica.BLL;
+using Modelo.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class IngresoController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult LeerTodo(int cantidad = 10, int pagina = 0, string textoBusqueda = null)
+        {
+            var respuesta = new RespuestaVMR<ListadoPaginadoVMR<IngresoVMR>>();
+
+            try
+            {
+                respuesta.datos = IngresoBLL.LeerTodo(cantidad, pagina, textoBusqueda);
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = null;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+        [HttpGet]
+        public IHttpActionResult LeerUno(long id)
+        {
+            var respuesta = new RespuestaVMR<IngresoVMR>();
+
+            try
+            {
+                respuesta.datos = IngresoBLL.LeerUno(id);
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = null;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+            if (respuesta.datos == null && respuesta.mensajes.Count() == 0)
+            {
+                respuesta.codigo = HttpStatusCode.NotFound;
+                respuesta.mensajes.Add("Elemento no encontrado");
+            }
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult Crear(Ingreso item)
+        {
+            var respuesta = new RespuestaVMR<long?>();
+
+            try
+            {
+                respuesta.datos = IngresoBLL.Crear(item);
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = null;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(long id, IngresoVMR item)
+        {
+            var respuesta = new RespuestaVMR<bool?>();
+
+            try
+            {
+                item.id = id;
+                IngresoBLL.Actualizar(item);
+                respuesta.datos = true;
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = false;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+
+        [HttpDelete]
+        public IHttpActionResult Eliminar(List<long> ids)
+        {
+            var respuesta = new RespuestaVMR<bool?>();
+
+            try
+            {
+                IngresoBLL.Eliminar(ids);
+                respuesta.datos = true;
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = false;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+
+    }
+}
