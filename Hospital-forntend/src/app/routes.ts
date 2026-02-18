6891cd769f3ad6ec7254f81d7e8c78b4cbb6c399
Yeu <yeumensilva9@gmail.com>
2026-02-18 16:07:08 -0300

    redirect home and medic module

--- Hospital-forntend/src/app/routes.ts
+++ Hospital-forntend/src/app/routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
 import { globalRoutes } from "./modules/global/global.routing";
+import { medicoRoutes } from "./modules/medico/medico.routes";
 
 export const RUTAS: Routes = [
     ...globalRoutes,
+    ...medicoRoutes
 ];
